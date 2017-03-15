046bca76f05e0a9b55b66cf576ce0fe9ce7bf7d3
Rafael Pereira <rafaelbsides@gmail.com>
2017-03-15 12:33:05 -0300

    Fundamentos básicos adicionados

--- src/app/main.js
+++ src/app/main.js
@@ -3,8 +3,26 @@ var testEscale = angular.module('app');
 testEscale
   .component('app', {
     templateUrl: 'app/main.html',
-    controller: function () {
-      this.main = 'Hello World!';
-      this.hello = 'Hello';
-    }
+    controller: 'mainCtrl'
   });
+
+testEscale.factory('GHAPI', function($http, $q) {
+  var url = {
+    base: 'https://api.github.com/users/wilfernandesjr',
+    stars: '/starred',
+  };
+
+  return {
+    getStars: function() {
+      return $http.get(url.base + url.stars).then(function(response) { return response.data });
+    }
+  };
+});
+
+testEscale.controller('mainCtrl', function($scope, GHAPI) {
+  GHAPI.getStars().then(function(data) {
+    $scope.stars = data;
+  }, function() {
+    $scope.error = 'Não foi possível se conectar ao Github :(';
+  });
+});
