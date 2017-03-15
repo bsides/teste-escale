544b6dd078010bb54d5ce0a3cce432893c4c4ea8
Rafael Pereira <rafaelbsides@gmail.com>
2017-03-15 13:24:13 -0300

    Funcionalidades básicas feitas

--- src/app/main.js
+++ src/app/main.js
@@ -7,15 +7,12 @@ testEscale
   });
 
 testEscale.factory('GHAPI', function($http, $q) {
-  var url = {
-    base: 'https://api.github.com/users/wilfernandesjr',
-    stars: '/starred',
-  };
-
   return {
     getStars: function() {
-      return $http.get(url.base + url.stars).then(function(response) { return response.data });
-    }
+      return $http.get('https://api.github.com/users/wilfernandesjr/starred').then(function(response) {
+        return response.data
+      });
+    },
   };
 });
 
@@ -25,4 +22,23 @@ testEscale.controller('mainCtrl', function($scope, GHAPI) {
   }, function() {
     $scope.error = 'Não foi possível se conectar ao Github :(';
   });
+
+  $scope.filtering = function() { console.log('alo') };
+});
+
+testEscale.filter('unique', function() {
+  return function(collection, keyname) {
+    var output = [],
+        keys = [];
+
+    angular.forEach(collection, function(item) {
+      var key = item[keyname];
+      if(keys.indexOf(key) === -1) {
+        keys.push(key);
+        output.push(item);
+      }
+    });
+
+    return output;
+  };
 });
